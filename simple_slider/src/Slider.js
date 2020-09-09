328c51d1b743e65442a824e221dac7933e8014cf
Jeisson <jeibogot@publicisgroupe.net>
2020-09-08 19:50:50 -0500

    Adding missing logic for infinite loops

--- simple_slider/src/Slider.js
+++ simple_slider/src/Slider.js
@@ -1,75 +1,7 @@
 import React, { Component } from 'react';
-import { getSlides, getStyles } from './utils';
-import styled from "styled-components";
+import { StyledWrapper, StyledSlider, getSlides, Slide, Dots, Arrow } from './utils';
 
 const slides = getSlides();
-const StyledWrapper = getStyles().StyledWrapper;
-const StyledDots = getStyles().StyledDots;
-
-const StyledSlider = styled.div`
-transform: translateX(-${props => props.translate}px);
-transition: transform ease-out ${props => props.transition}s;
-height: 100%;
-width: ${props => props.width}px;
-display: flex;
-`;
-
-const StyledSlide = styled.div`
-flex: 0 0 100%;
-height: 100%;
-width: 100%;
-background-image: url('${props => props.content}');
-background-size: cover;
-background-repeat: no-repeat;
-background-position: center;
-`;
-
-const StyledArrow = styled.div`
-color: black;
-display: flex;
-position: absolute;
-top: 50%;
-${props => props.direction === 'right' ? `right: 25px` : `left: 25px`};
-height: 50px;
-width: 50px;
-justify-content: center;
-background: white;
-border-radius: 50%;
-cursor: pointer;
-align-items: center;
-transition: transform ease-in 0.1s;
-&:hover {
-  transform: scale(1.1);
-}
-img {
-  transform: translateX(${props => props.direction === 'left' ? '-2' : '2'}px);
-  &:focus {
-    outline: 0;
-  }
-}`;
-
-const Dot = styled.div`
-padding: 10px;
-margin-right: 5px;
-cursor: pointer;
-border-radius: 50%;
-background: ${props => props.active ? 'black' : 'white'};
-`;
-
-const Slide = ({ content }) => (
-  <StyledSlide content={content} />
-)
-
-const Dots = ({ activeIndex, handleClick }) => (
-  <StyledDots>
-    {slides.map((slide, i) => (
-      <Dot key={slide} active={activeIndex === i} onClick={() => handleClick(i)} />
-    ))}
-  </StyledDots>
-)
-
-const Arrow = ({ direction, handleClick }) =>
-  <StyledArrow direction={direction} onClick={handleClick}>{direction === "left" ? "<" : ">"}</StyledArrow>;
 
 class Slider extends Component {
   constructor(props) {
@@ -151,7 +83,7 @@ class Slider extends Component {
       translate: this.getWidth(),
       transition: 0,
       slides: images
-    });
+    }, () => { console.log("hello there") });
   }
 
   render() {
--- simple_slider/src/SliderFunc.jsx
+++ simple_slider/src/SliderFunc.jsx
@@ -1,121 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { getSlides, getStyles } from './utils';
-import styled from "styled-components";
+import React, { useState, useEffect, useRef } from 'react';
+import { StyledWrapper, getSlides, StyledSlider, Slide, Dots, Arrow } from './utils';
+
+const slides = getSlides();
+
+const getWidth = () => window.innerWidth;
 
 function SliderFunc() {
 
-  const StyledSlide = styled.div`
-  flex: 0 0 100%;
-  height: 100%;
-  width: 100%;
-  background-image: url('${props => props.content}');
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  `;
-
-  const Dot = styled.div`
-  padding: 10px;
-  margin-right: 5px;
-  cursor: pointer;
-  border-radius: 50%;
-  background: ${props => props.active ? 'black' : 'white'};
-  `;
-
-  const StyledArrow = styled.div`
-  color: black;
-  display: flex;
-  position: absolute;
-  top: 50%;
-  ${props => props.direction === 'right' ? `right: 25px` : `left: 25px`};
-  height: 50px;
-  width: 50px;
-  justify-content: center;
-  background: white;
-  border-radius: 50%;
-  cursor: pointer;
-  align-items: center;
-  transition: transform ease-in 0.1s;
-  &:hover {
-    transform: scale(1.1);
-  }
-  img {
-    transform: translateX(${props => props.direction === 'left' ? '-2' : '2'}px);
-    &:focus {
-      outline: 0;
-    }
-  }`;
-
-  const StyledSlider = styled.div`
-  transform: translateX(-${props => props.translate}px);
-  transition: transform ease-out ${props => props.transition}s;
-  height: 100%;
-  width: ${props => props.width}px;
-  display: flex;
-  `;
-
-  const slides = getSlides();
-  const StyledWrapper = getStyles().StyledWrapper;
-  const StyledDots = getStyles().StyledDots;
-
   const [timer, setTimer] = useState(0);
-  const [width, setWidth] = useState(window.innerWidth);
+  const activeIndexRef = useRef(0);
   const [activeIndex, setActiveIndex] = useState(0);
-  const [translate, setTranslate] = useState(window.innerWidth);
+  const [translate, setTranslate] = useState(getWidth());
   const [transition, setTransition] = useState(0.5);
   const [slidesForView, setSlides] = useState([slides[slides.length - 1], slides[0], slides[1]]);
 
   useEffect(() => {
-    setTimer(setInterval(() => { nextSlide() }, 3000));
-    const transitionEnd = window.addEventListener('transitionend', createSlides());
+    setTimer(setInterval(() => { nextSlide() }, 300000));
+    const transitionEnd = window.addEventListener('transitionend', createSlides);
     setSlides(createSlides());
-    console.log(width);
+    console.log(getWidth());
+
     return () => {
       clearInterval(timer);
       window.removeEventListener('transitionend', transitionEnd);
     }
-  });
+
+  }, []);
 
   const createSlides = () => {
     let images = [];
-    if (activeIndex === slides.length - 1) {
+    if (activeIndexRef.current === slides.length - 1) {
       images = [slides[slides.length - 2], slides[slides.length - 1], slides[0]];
-    } else if (activeIndex === 0) {
+    } else if (activeIndexRef.current === 0) {
       images = [slides[slides.length - 1], slides[0], slides[1]];
     } else {
-      images = slides.slice(activeIndex - 1, activeIndex + 2)
+      images = slides.slice(activeIndexRef.current - 1, activeIndexRef.current + 2)
     }
     setTranslate(getWidth());
     setTransition(0);
+    console.log("createSlides -> images", activeIndexRef.current);
+    console.log("createSlides -> translate", translate);
     return images;
   }
 
-  useEffect(() => {
-    console.log(width);
-  }, [width]);
-
-  const getWidth = () => {
-    setWidth(window.innerWidth);
-  };
-
-  const Slide = ({ content }) => (
-    <StyledSlide content={content} />
-  )
-
-  const Dots = ({ activeIndex, handleClick }) => (
-    <StyledDots>
-      {slides.map((slide, i) => (
-        <Dot key={slide} active={activeIndex === i} onClick={() => handleClick(i)} />
-      ))}
-    </StyledDots>
-  )
-
-  const Arrow = ({ direction, handleClick }) => (
-    <StyledArrow direction={direction} onClick={handleClick}>{direction === "left" ? "<" : ">"}</StyledArrow>
-  )
-
   const nextSlide = () => {
-    setActiveIndex(activeIndex === slides.length - 1 ? 0 : activeIndex + 1);
+    activeIndexRef.current++;
+    setActiveIndex(1);
     setTransition(0.5);
     setTranslate(translate + getWidth())
   }
@@ -126,11 +56,11 @@ function SliderFunc() {
         Hello there
       </div>
       {<StyledWrapper>
-        <StyledSlider width={width} translate={translate} transition={transition}>
+        <StyledSlider width={getWidth()} translate={translate} transition={transition}>
           {slidesForView.map((slide, i) => {
             console.log("slidesForView -> slide", slide, slidesForView.length);
             return (
-              <Slide key={slide + i} content={slide} width={getWidth} />
+              <Slide key={slide + i} content={slide} width={getWidth()} />
             )
           })}
         </StyledSlider>
